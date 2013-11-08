7a3ed0bfb4ca72a84a7c8758e4f3d6bb7349f2e7
Henry <cryptix@riseup.net>
2013-11-08 14:58:17 +0200

    moved wp.watch() to api.go

--- api.go
+++ api.go
@@ -303,3 +303,44 @@ func (wp *PshdlWorkspace) parseApiRequest(req *http.Request) error {
 	}
 	return nil
 }
+
+func (wp *PshdlWorkspace) watch(dir string) {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		panic(err)
+	}
+	done := make(chan bool)
+
+	// Process events
+	go func() {
+		for {
+			select {
+			case ev := <-watcher.Event:
+				if strings.HasSuffix(ev.Name, ".pshdl") {
+					fmt.Println("PSHDL Code! Adding", ev.Name)
+					err := wp.AddFile(ev.Name)
+					if err != nil {
+						panic(err)
+					}
+				}
+				// fmt.Println("event:", ev)
+			case err := <-watcher.Error:
+				fmt.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Watch(dir)
+	if err != nil {
+		if os.IsExist(err) {
+			fmt.Printf("Invalid Watch Directory..")
+			os.Exit(1)
+		}
+		panic(err)
+	}
+
+	<-done
+
+	/* ... do stuff ... */
+	watcher.Close()
+}
