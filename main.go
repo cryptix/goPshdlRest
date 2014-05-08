806896535b4d60afbcf896e2a49115ca36a98a0e
Henry <cryptix@riseup.net>
2014-05-08 19:57:02 +0200

    refurbished

--- main.go
+++ main.go
@@ -1,179 +1,108 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"io"
+	"io/ioutil"
+	"log"
 	"os"
+	"path/filepath"
 	"strings"
-	"sync"
-	"text/template"
+
+	api "github.com/cryptix/goPshdlRest"
+	"github.com/howeyc/fsnotify"
 )
 
-// A Command is an implementation of a pshdl command
-// taken from https://code.google.com/p/go/source/browse/src/cmd/go/main.go
-type Command struct {
-	// Run runs the command.
-	// The args are the arguments after the command name.
-	Run func(cmd *Command, args []string)
-
-	// UsageLine is the one-line usage message.
-	// The first word in the line is taken to be the command name.
-	UsageLine string
-
-	// Short is the short description shown in the 'go help' output.
-	Short string
-
-	// Long is the long message shown in the 'go help <this-command>' output.
-	Long string
-
-	// Flag is a set of flags specific to this command.
-	Flag flag.FlagSet
-
-	// CustomFlags indicates that the command will do its own
-	// flag parsing.
-	CustomFlags bool
-}
-
-// Name returns the command's name: the first word in the usage line.
-func (c *Command) Name() string {
-	name := c.UsageLine
-	i := strings.Index(name, " ")
-	if i >= 0 {
-		name = name[:i]
-	}
-	return name
-}
-
-func (c *Command) Usage() {
-	fmt.Fprintf(os.Stderr, "usage: %s\n\n", c.UsageLine)
-	fmt.Fprintf(os.Stderr, "%s\n", strings.TrimSpace(c.Long))
-	os.Exit(2)
-}
-
-// Commands lists the available commands and help topics.
-// The order here is the order in which they are printed by 'go help'.
-var commands = []*Command{
-	cmdOpen,
-	cmdNew,
-}
-
-var exitStatus = 0
-var exitMu sync.Mutex
-
-func setExitStatus(n int) {
-	exitMu.Lock()
-	if exitStatus < n {
-		exitStatus = n
-	}
-	exitMu.Unlock()
-}
-
 func main() {
-	flag.Usage = usage
-	flag.Parse()
-	// log.SetFlags(0)
+	var wp *api.Workspace
+	client := api.NewClient(nil)
 
-	args := flag.Args()
-	if len(args) < 1 {
-		usage()
-	}
+	// check if we have a workspace id file
+	widFile, err := os.Open(".wid")
+	if err == nil {
+		wid, err := ioutil.ReadAll(widFile)
 
-	if args[0] == "help" {
-		help(args[1:])
-		return
-	}
+		client = api.NewClient(nil)
+		client.Workspace.ID = string(wid)
+		client.Compiler.ID = string(wid)
 
-	for _, cmd := range commands {
-		if cmd.Name() == args[0] && cmd.Run != nil {
-			cmd.Flag.Usage = func() { cmd.Usage() }
-			if cmd.CustomFlags {
-				args = args[1:]
-			} else {
-				cmd.Flag.Parse(args[1:])
-				args = cmd.Flag.Args()
-			}
-			cmd.Run(cmd, args)
-			exit()
-			return
+		wp, _, err = client.Workspace.GetInfo()
+		if err != nil {
+			log.Fatalf("Workspace.GetInfo() API Error: %s\n", err)
+		}
+		log.Printf("Workspace Opened:%s\nFiles:%v\n", wp.ID, wp.Files)
+
+		// todo check if files allready there
+		err = client.Workspace.DownloadAllFiles()
+		if err != nil {
+			log.Fatalf("Workspace.DownloadAllFiles() API Error: %s\n", err)
+		}
+		log.Printf("Download of PSHDL-Code complete")
+
+	} else if os.IsNotExist(err) {
+		log.Println("No <.wid> file, create new workspace")
+
+		wp, _, err = client.Workspace.Create()
+		if err != nil {
+			log.Fatalf("Workspace.Create() API Error: %s\n", err)
+		}
+		log.Println("Workspace Created:", wp.ID)
+
+	} else if err != nil {
+		if err != nil {
+			log.Fatalf("os.Open(.wid) Error: %s\n", err)
 		}
 	}
+	widFile.Close()
 
-	fmt.Fprintf(os.Stderr, "pshdlSync: unkown subcommand %q\nRun '%s help' for usage.\n", args[0])
-	setExitStatus(2)
-	exit()
-}
+	//todo push containing files
+	log.Println("Starting to watch..")
 
-var usageTemplate = `pshdlSync is a helper for interacting with the PSHDL rest API.
-
-Usage:
-	pshdlSync command [arguments]
-
-The commands are:
-{{range .}}
-	{{.Name | printf "%-11s"}} {{.Short}}{{end}}
-
-Use "pshdlSync help command" to get more information.
-`
-
-var helpTemplate = `usage: pshdlSync {{.UsageLine}}
-
-{{.Long | trim}}
-`
-
-func tmpl(w io.Writer, text string, data interface{}) {
-	t := template.New("top")
-	t.Funcs(template.FuncMap{"trim": strings.TrimSpace})
-	template.Must(t.Parse(text))
-	if err := t.Execute(w, data); err != nil {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
 		panic(err)
 	}
-}
+	done := make(chan bool)
 
-func printUsage(w io.Writer) {
-	tmpl(w, usageTemplate, commands)
-}
+	// Process events
+	go func() {
+		for {
+			select {
+			case ev := <-watcher.Event:
+				if strings.HasSuffix(ev.Name, ".pshdl") {
+					log.Println("PSHDL Code! Adding", ev.Name)
 
-func usage() {
-	printUsage(os.Stderr)
-	os.Exit(2)
-}
+					file, err := os.Open(ev.Name)
+					if err != nil {
+						log.Fatalf("os.Open error: %s\n", err)
+						done <- true
+						return
+					}
 
-// help implements the 'help' command.
-func help(args []string) {
-	if len(args) == 0 {
-		printUsage(os.Stdout)
-		// not exit 2: succeeded at 'go help'.
-		return
-	}
-	if len(args) != 1 {
-		fmt.Fprintf(os.Stderr, "usage: pshdlSync help command\n\nToo many arguments given.\n")
-		os.Exit(2) // failed at 'go help'
-	}
-
-	arg := args[0]
-
-	for _, cmd := range commands {
-		if cmd.Name() == arg {
-			tmpl(os.Stdout, helpTemplate, cmd)
-			// not exit 2: succeeded at 'go help cmd'.
-			return
+					err = client.Workspace.UploadFile(filepath.Base(ev.Name), file)
+					if err != nil {
+						log.Fatalf("UploadFile error: %s\n", err)
+						done <- true
+						return
+					}
+					file.Close()
+				}
+			case err := <-watcher.Error:
+				log.Println("error:", err)
+				done <- true
+			}
 		}
+	}()
+
+	err = watcher.Watch(".")
+	if err != nil {
+		if os.IsExist(err) {
+			log.Fatal("Invalid Watch Directory..")
+		}
+		panic(err)
 	}
 
-	fmt.Fprintf(os.Stderr, "Unknown help topic %#q.  Run 'pshdlSync help'.\n", arg)
-	os.Exit(2) // failed at 'go help cmd'
-}
+	<-done
 
-var atexitFuncs []func()
+	watcher.Close()
 
-func atexit(f func()) {
-	atexitFuncs = append(atexitFuncs, f)
-}
-
-func exit() {
-	for _, f := range atexitFuncs {
-		f()
-	}
-	os.Exit(exitStatus)
+	os.Exit(0)
 }
