1f8981ad23ada54d006be1d1f6dd68607f30faae
Henry <cryptix@riseup.net>
2014-05-09 15:31:17 +0200

    pshdlCompilat - cleared up logging a bit

--- cmd/pshdlCompilat/main.go
+++ cmd/pshdlCompilat/main.go
@@ -33,7 +33,7 @@ func main() {
 	}
 
 	if *streamVHDL {
-		log.Println("Csim download active")
+		log.Println("VHDL download active")
 	}
 
 	// check if we have a workspace id file
@@ -56,36 +56,35 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error: %s\n", err)
 	}
+	log.Println("EventStream open")
 
-	log.Println("Displaying events...")
 	for ev := range evChan {
 		subj := ev.GetSubject()
+		log.Println("[R]", subj)
+
 		switch {
 
 		case strings.HasPrefix(subj, "P:WORKSPACE:"):
-			log.Println("Worskpace Changed:", subj)
 			for _, file := range ev.GetFiles() {
 				log.Printf("[*] %s\n", file.RelPath)
 			}
 
 		case subj == "P:COMPILER:VHDL" && *streamVHDL:
-			log.Println("New VHDL Code")
-
 			err = client.Workspace.DownloadRecords(ev.GetFiles())
 			if err != nil {
 				log.Fatalf("Workspace.DownloadRecords() Error:. %s", err)
 				break
 			}
-			log.Println("[*] Download finished..")
+			log.Println("[*] VHDL Download finished..")
 
 		case subj == "P:COMPILER:C" && *streamCSim:
-			log.Println("New C-Sim Code")
 			err = client.Workspace.DownloadRecords(ev.GetFiles())
 			if err != nil {
 				log.Fatalf("Could not load all files. %s", err)
 				break
 			}
-			log.Println("[*] Download finished..")
+			log.Println("[*] CSim Download finished..")
 		}
+
 	}
 }
